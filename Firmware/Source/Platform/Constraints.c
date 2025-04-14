027db20724a417b19206b650f0ca4bcfab130aa8
Alexander-Mamasaklisov <a.mamasakhlisov@proton-electrotex.com>
2025-04-14 12:33:42 +0300

    Добавлены Constraints для добавленых раннее регистров

--- Firmware/Source/Platform/Constraints.c
+++ Firmware/Source/Platform/Constraints.c
@@ -128,7 +128,7 @@ const TableItemConstraint NVConstraint[DATA_TABLE_NV_SIZE] = {
 		{0, INT16U_MAX, 0},																		// 122
 		{GAIN_COEFFICIENT_MIN, GAIN_COEFFICIENT_MAX, GAIN_COEFFICIENT_DEF},						// 123
 		{0, INT16U_MAX, 0},																		// 124
-		{0, 0, 0},																				// 125
+		{0, INT16U_MAX, 0},																		// 125
 		{0, 0, 0},																				// 126
 		{0, 0, 0}																				// 127
 };
@@ -158,8 +158,8 @@ const TableItemConstraint VConstraint[DATA_TABLE_WP_START - DATA_TABLE_WR_START]
 		{0, 0, 0},																				// 149
 		{0, INT16U_MAX, 0},																		// 150
 		{0, INT16U_MAX, 0},																		// 151
-		{0, 0, 0},																				// 152
-		{0, 0, 0},																				// 153
+		{0, INT16U_MAX, 0},																		// 152
+		{0, INT16U_MAX, 0},																		// 153																				// 153
 		{0, 0, 0},																				// 154
 		{0, 0, 0},																				// 155
 		{0, 0, 0},																				// 156
